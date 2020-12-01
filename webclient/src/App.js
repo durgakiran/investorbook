12398a047cf54c94e2360e28612c3e31756f5ca6
durgakiran <gatemefuture@gmail.com>
2020-12-01 13:07:19 +0530

    Add basic theme && material ui && router

--- webclient/src/App.js
+++ webclient/src/App.js
@@ -1,12 +1,43 @@
-import React from 'react';
-import './App.css';
-import Investors from './Investors'
+import React from "react";
+import { createMuiTheme } from "@material-ui/core/styles";
+import { ThemeProvider } from "@material-ui/styles";
+import "./App.css";
+import Investors from "./Pages/Investors/Investors";
+import { BrowserRouter as Router } from "react-router-dom";
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      // light: will be calculated from palette.primary.main,
+      main: "#434FBC",
+      // dark: will be calculated from palette.primary.main,
+      // contrastText: will be calculated to contrast with palette.primary.main
+    },
+    secondary: {
+      light: "#0066ff",
+      main: "#0044ff",
+      // dark: will be calculated from palette.secondary.main,
+      contrastText: "#ffcc00",
+    },
+    // Used by `getContrastText()` to maximize the contrast between
+    // the background and the text.
+    contrastThreshold: 3,
+    // Used by the functions below to shift a color's luminance by approximately
+    // two indexes within its tonal palette.
+    // E.g., shift from Red 500 to Red 300 or Red 700.
+    tonalOffset: 0.2,
+  },
+});
 
 function App() {
   return (
-    <div className="App">
-      <Investors />
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <Router>
+          <Investors />
+        </Router>
+      </div>
+    </ThemeProvider>
   );
 }
 
