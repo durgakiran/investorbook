38e191fbf884e1b750b68f904c2ec055fa012ba4
durgakiran <gatemefuture@gmail.com>
2020-12-01 18:49:55 +0530

    Add Container css

---
+++ webclient/src/Components/Title/Title.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import logo from './Company-logo.png';
+
+export default function Title() {
+    return (
+        <div className="title">
+            <img src={logo} alt="company logo" />
+        </div>
+    )
+}
+
--- webclient/src/Pages/Container/Container.js
+++ webclient/src/Pages/Container/Container.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import Title from '../../Components/Title/Title';
 import InvestorProfile from '../Investors/InvestorProfile/InvestorProfile';
 import TableContainer from '../TableContainer/TableContainer';
-
+import  styles from './Container.module.css'
 
 export default function Container() {
     return (
-        <div className="container">
+        <div className={styles.container}>
+            <Title />
+            
             <Switch>
                 <Route path="/:id" children={<InvestorProfile />} />
                 <Route path="/" children={<TableContainer />} />
